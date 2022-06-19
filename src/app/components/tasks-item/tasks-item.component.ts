58317f9fbd8efb9e2bbe80f0eba0b0bfd0c4a69e
Leonardo Alvarez <leo92alvarez25@gmail.com>
2022-06-19 00:33:12 -0300

    style and icon tasks

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks-item',
@@ -11,6 +12,9 @@ import { TASKS } from 'src/app/mock-tasks';
 export class TasksItemComponent implements OnInit {
 
   @Input()task: Task= TASKS[0]
+
+  faTimes=faTimes
+  delete={color:'#C91265'};
 
   constructor() { }
 
